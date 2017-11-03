3743fd7ec5ed44fd4c021c41985043b1c637a511
Vlad Didenko <git@didenko.com>
2017-11-02 20:34:26 -0500

    Clean up GoDoc formatting

--- tree_copy.go
+++ tree_copy.go
@@ -1,17 +1,6 @@
 // Copyright 2017 Vlad Didenko. All rights reserved.
 // See the included LICENSE.md file for licensing information
 
-// Package fstest is a collection of functions to help
-// testing filesyste objects modifications. It focuses
-// on creating and cleaning up a baseline filesyetem state.
-//
-// The following are addressed use cases:
-//
-// 1. Create a directory hierarchy via an API
-// 2. Create a directory hierarchy via a copy of a template
-// 3. Write a provided test data to files
-// 4. Contain all test activity in a temporatry directory
-// 5. Compare two directories recursively
 package fstest // import "go.didenko.com/fstest"
 
 import (
