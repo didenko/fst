41a3f2ca03c6c15ad9f5e4ef8d7f163df9e42081
Vlad Didenko <git@didenko.com>
2017-11-07 14:57:28 -0500

    Tighter permissons, reasonable dates, no unneded UTC calls.

--- dir_create_test.go
+++ dir_create_test.go
@@ -29,7 +29,7 @@ func ExampleDirCreate() {
 	log.Printf("Temp folder: %v\n", wd)
 
 	dirs := `
-2999-01-01T01:01:01Z 0777 dir_create_example
+2999-01-01T01:01:01Z 0700 dir_create_example
 
 2001-01-01T01:01:01Z 0750 dir_create_example/a
 2001-01-01T01:01:01Z 0750 dir_create_example/b
@@ -58,21 +58,21 @@ func ExampleDirCreate() {
 func TestDirCreate(t *testing.T) {
 
 	dirs := `
-2001-01-01T01:01:01Z 0777 aaa
-2009-01-01T01:01:01Z 0777 aaa/bbb
+2001-01-01T01:01:01Z 0150 aaa
+2099-01-01T01:01:01Z 0700 aaa/bbb
 
-2002-01-01T01:01:01Z 0777 "has\ttab"
-2002-01-01T01:01:01Z 0777 "\u10077heavy quoted\u10078"`
+2002-01-01T01:01:01Z 0700 "has\ttab"
+2002-01-01T01:01:01Z 0700 "\u10077heavy quoted\u10078"`
 
 	expect := []struct {
 		t time.Time
 		p os.FileMode
 		n string
 	}{
-		{time.Date(2001, time.January, 1, 1, 1, 1, 0, time.UTC), 0777, "aaa"},
-		{time.Date(2002, time.January, 1, 1, 1, 1, 0, time.UTC), 0777, "has\ttab"},
-		{time.Date(2002, time.January, 1, 1, 1, 1, 0, time.UTC), 0777, "\u10077heavy quoted\u10078"},
-		{time.Date(2009, time.January, 1, 1, 1, 1, 0, time.UTC), 0777, "aaa/bbb"},
+		{time.Date(2001, time.January, 1, 1, 1, 1, 0, time.UTC), 0150, "aaa"},
+		{time.Date(2002, time.January, 1, 1, 1, 1, 0, time.UTC), 0700, "has\ttab"},
+		{time.Date(2002, time.January, 1, 1, 1, 1, 0, time.UTC), 0700, "\u10077heavy quoted\u10078"},
+		{time.Date(2099, time.January, 1, 1, 1, 1, 0, time.UTC), 0700, "aaa/bbb"},
 	}
 
 	root, cleanup, err := InitTempDir()
@@ -110,7 +110,7 @@ func TestDirCreate(t *testing.T) {
 		if fi.Name() != expect[i].n {
 			t.Errorf("Names mismatch, expected \"%v\", got \"%v\"", expect[i].n, fi.Name())
 		}
-		if !fi.ModTime().UTC().Equal(expect[i].t) {
+		if !fi.ModTime().Equal(expect[i].t) {
 			t.Errorf("Times mismatch, expected \"%v\", got \"%v\" for \"%v\"", expect[i].t, fi.ModTime().UTC(), expect[i].n)
 		}
 		if fi.Mode().Perm() != expect[i].p {
@@ -124,7 +124,7 @@ func TestDirCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if !fi.ModTime().UTC().Equal(expect[3].t) {
+	if !fi.ModTime().Equal(expect[3].t) {
 		t.Errorf("Times mismatch, expected \"%v\", got \"%v\" for \"%v\"", expect[3].t, fi.ModTime().UTC(), expect[3].n)
 	}
 
