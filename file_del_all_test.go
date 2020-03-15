55d1ed6adf2312dfb7ff998f8033a74235d5bbfd
Vlad Didenko <git@didenko.com>
2020-03-14 23:00:01 -0700

    FileDelAll test failures

--- file_del_all_test.go
+++ file_del_all_test.go
@@ -4,6 +4,7 @@
 package fst // import "go.didenko.com/fst"
 
 import (
+	"os"
 	"testing"
 )
 
@@ -37,4 +38,42 @@ func TestFileDelAll(t *testing.T) {
 	if len(diffs) > 0 {
 		t.Errorf("Tree foes not match expected after FileDelAll:\n%v\n", diffs)
 	}
+}
+
+func TestFileDelAllWalkError(t *testing.T) {
+	nodes := []*Node{
+		&Node{0750, Rfc3339(t, "2018-01-01T01:01:01Z"), "mock/", ""},
+		&Node{0750, Rfc3339(t, "2018-03-04T05:06:07Z"), "mock/dir/", ""},
+		&Node{0640, Rfc3339(t, "2018-04-05T06:07:08Z"), "mock/dir/.gitkeep", ""},
+		&Node{0640, Rfc3339(t, "2018-02-03T04:05:06Z"), "mock/dir/text.txt", ""},
+	}
+
+	_, cleanup := TempCreateChdir(t, nodes)
+	defer cleanup()
+
+	if err := os.Chmod("mock/dir", 0644); err != nil {
+		t.Errorf("Test setup failed to change directory permissions to u-x: %v", err)
+	}
+
+	testWrapper := wrapFatalfTest(t, "Remove: while walking to \"mock/dir/.gitkeep\": ")
+	FileDelAll(testWrapper, "mock", ".gitkeep")
+}
+
+func TestFileDelAllRemoveError(t *testing.T) {
+	nodes := []*Node{
+		&Node{0750, Rfc3339(t, "2018-01-01T01:01:01Z"), "mock/", ""},
+		&Node{0750, Rfc3339(t, "2018-03-04T05:06:07Z"), "mock/dir/", ""},
+		&Node{0640, Rfc3339(t, "2018-04-05T06:07:08Z"), "mock/dir/.gitkeep", ""},
+		&Node{0640, Rfc3339(t, "2018-02-03T04:05:06Z"), "mock/dir/text.txt", ""},
+	}
+
+	_, cleanup := TempCreateChdir(t, nodes)
+	defer cleanup()
+
+	if err := os.Chmod("mock/dir", 0550); err != nil {
+		t.Errorf("Test setup failed to change directory permissions to u-w: %v", err)
+	}
+
+	testROWrap := wrapFatalfTest(t, "Removing \"mock/dir/.gitkeep\": ")
+	FileDelAll(testROWrap, "mock", ".gitkeep")
 }
