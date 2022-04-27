bfa05c7fa68f3e71f0ad98703d68023f085e67f9
Vlad Didenko <git@didenko.com>
2022-04-27 05:31:33 -0700

    added copyright notice

---
+++ fst_godoc.go
@@ -0,0 +1,19 @@
+// Copyright 2017-2022 Vlad Didenko. All rights reserved.
+// See the included LICENSE.md file for licensing information
+
+// Package fst is a collection of functions to help
+// testing filesyste objects modifications. It focuses
+// on creating and cleaning up a baseline filesyetem state.
+//
+// The following are addressed use cases:
+//
+// 1. Create a directory hierarchy via an API
+//
+// 2. Create a directory hierarchy via a copy of a template
+//
+// 3. Write a provided test mock data to files
+//
+// 4. Contain all test activity in a temporatry directory
+//
+// 5. Compare two directories recursively
+package fst // import "go.didenko.com/fst"
